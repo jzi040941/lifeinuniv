76d69190f07be35d2a9129473854e3b9da79c73c
jzi040941 <jzi040941@naver.com>
2016-10-21 19:18:52 +0900

    hello

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#pragma strict
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.orientation = ScreenOrientation.Portrait;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+ 
+	public void LoadScene () {
+		Application.LoadLevel ("main_screen");
+	}
+}
---
+++ Assets/popup_toggle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class popup_toggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Example() {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
