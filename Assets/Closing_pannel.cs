669b2ea0dbc4c6e5d2327ccd017cbe6f5b267b4f
jzi040941 <jzi040941@naver.com>
2016-11-20 16:21:26 +0900

    activity 하면 수치 증감 하게 변경하고, 그림도 나오게 함.

--- Assets/Closing_pannel.cs
+++ Assets/Closing_pannel.cs
@@ -5,43 +5,74 @@ public class Closing_pannel : MonoBehaviour
 {
     MainStatusCtr mainStatusCtr;
     GameObject ui_instance;
-
+	Image myImageComponent;
+	Sprite first;
+	public Sprite second;
 
     public GameObject[] inactive;
     void Start()
     {
 		inactive = GameObject.FindGameObjectsWithTag ("uninteratible");
+		myImageComponent = GetComponent<Image>(); 
+		first = myImageComponent.sprite;
 
         ui_instance = GameObject.Find("UI");
         mainStatusCtr = ui_instance.GetComponent<MainStatusCtr>();
     }
 
+	void changetosecond(){
+		myImageComponent.sprite = second;
+	}
     public void invokereset()
     {
+		Invoke ("changetosecond", 1.5f);
         Invoke("Closing", 3);
     }
 
     void Closing()
     {
-        gameObject.SetActive(false);
+        
         GameObject.Find("activity_Panel").SetActive(false);
 
         mainStatusCtr.Act = mainStatusCtr.Act - 1;
         //      Debug.Log("remain " + mainStatusCtr.Act + "Act");
 
-        if (gameObject.name == "Class_Panel")
-        {
-            mainStatusCtr.Job = mainStatusCtr.Job + 10;
-        }
 
-        if (gameObject.name == "Part-time_Panel")
-        {
-            mainStatusCtr.Attr = mainStatusCtr.Attr + 10;
-        }
-
+		this.chanagestatus (gameObject.name);
+			
         foreach (GameObject a in inactive){
 			Button btn = a.GetComponent<Button> ();
 			btn.interactable = true;
 		}
-    } 
+		gameObject.SetActive(false);
+		myImageComponent.sprite = first;
+    }
+
+	void chanagestatus(string name){
+		if (name == "Class_Panel")
+		{
+			mainStatusCtr.Health -= Random.Range(5,15);
+			mainStatusCtr.Job = mainStatusCtr.Job + Random.Range(5,15);
+		}
+
+		if (name == "Study_Panel")
+		{
+			mainStatusCtr.Health -= Random.Range (5, 15);
+			mainStatusCtr.Job += Random.Range (3, 8);
+			mainStatusCtr.Attr += Random.Range (3, 8);
+			mainStatusCtr.Money -= Random.Range (30, 101);
+		}
+
+		if (name == "Drinking_Panel")
+		{
+			mainStatusCtr.Health -= Random.Range (15, 31);
+			mainStatusCtr.Money -= Random.Range (200, 401);
+		}
+
+		if (name == "Part-time_Panel")
+		{
+			mainStatusCtr.Health -= Random.Range (10, 21);
+			mainStatusCtr.Money += Random.Range (100, 301);
+		}
+	}
 }
