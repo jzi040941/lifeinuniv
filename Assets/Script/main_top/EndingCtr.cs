e0e1c49c5f792f0cae8ea3c8c54843ec8a95bfb4
LAPTOP-FU45AV1U\김태훈 <kitsch94@gmail.com>
2016-11-25 22:17:50 +0900

    종료할 때 Status 저장하는 Script 추가 / Ending Popup 작성중

---
+++ Assets/Script/main_top/EndingCtr.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndingCtr : MonoBehaviour {
+    MainStatusCtr mainStatusCtr;
+    GameObject ui_instance;
+    GameObject healthZeroPopup,stonePopup,failPopup,drunkenPopup;
+
+    // Use this for initialization
+    void Start()
+    {
+        ui_instance = GameObject.Find("UI");
+        mainStatusCtr = ui_instance.GetComponent<MainStatusCtr>();
+        healthZeroPopup = GameObject.Find("Ending_HealthZero");
+        stonePopup = GameObject.Find("Ending_LibraryStone");
+        failPopup = GameObject.Find("Ending_GradFail");
+        drunkenPopup = GameObject.Find("Ending_Drunken");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    void restart_Init(){
+        mainStatusCtr.Health = 100;
+        mainStatusCtr.Job = 50;
+        mainStatusCtr.Attr = 50;
+        mainStatusCtr.Week = 1;
+        mainStatusCtr.Semester = 1;
+        mainStatusCtr.Act = 3;
+        mainStatusCtr.Year = 1;
+    }
+}
