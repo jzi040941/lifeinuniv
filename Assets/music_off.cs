693059c778ec33c8331e1f46fcf1ea965c643fc3
DESKTOP-I79Q8O0\user <yunyounglee1@gmail.com>
2016-11-28 00:30:21 +0900

    sound image변경 등등

---
+++ Assets/music_off.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class music_off : MonoBehaviour {
+
+    // Use this for initialization
+    AudioSource audio;
+
+	void Start ()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void musicoff()
+    {
+        audio.Stop();
+    }
+}
+
